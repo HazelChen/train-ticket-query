0972c490e861bc1b01aada8f185239e2d8fbb475
Lin <linchen7@foxmail.com>
2017-01-11 16:27:16 +0800

    feat:新增数据库以及填写数据到数据库的脚本

---
+++ add_queriers.py
@@ -0,0 +1,47 @@
+# -*- coding: UTF-8 -*-
+import sys, getopt, pymysql
+import station_code, seat_code
+
+def print_usage():
+    print('格式：python add_queriers.py -f 福州 -t 杭州 -d 2017-01-20')
+    print('或：python add_queriers.py -f 福州 -t 杭州 -d 2017-01-20 -n D3343,G366')
+    print('或：python add_queriers.py -f 福州 -t 杭州 -d 2017-01-20 -n D3343,G366 -s 一等座,无座')
+
+# 获取参数
+try:
+    options,args = getopt.getopt(sys.argv[1:],"hf:t:d:n:s:",["help","from=", "to=", "date=", "number=", "seat="])
+except getopt.GetoptError:
+    print_usage()
+    sys.exit()
+
+# 参数获取
+global from_station
+global to_station
+global query_date
+valid_trips = None
+valid_seats = None
+
+for option, arg in options:
+    if option in ("-h", "help"):
+        print_usage()
+        sys.exit()
+    if option in ("-f", "from"):
+        from_station = station_code.code_map[arg]
+    if option in ("-t", "to"):
+        to_station = station_code.code_map[arg]
+    if option in ("-d", "date"):
+        query_date = arg
+    if option in ("-n", "number"):
+        valid_trips = arg
+    if option in ("-s", "seat"):
+        valid_seats = arg.split(',')
+        for num in range(len(valid_seats)):
+            valid_seats[num] = seat_code.code_map[valid_seats[num]]
+        valid_seats = ','.join(valid_seats)
+
+#将con设定为全局连接
+db = pymysql.connect('localhost', 'root', 'root', 'trainquery');
+with db:
+    #获取连接的cursor，只有获取了cursor，我们才能进行各种操作
+    cursor = db.cursor()
+    cursor.execute("INSERT INTO trainquery.querylist (`from`, `to`, `date`, `number`, `seat`, `status`) VALUES (%s, %s, %s, %s, %s, DEFAULT)", [from_station, to_station, query_date, valid_trips, valid_seats])
